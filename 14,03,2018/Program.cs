c9fa6c996accc7f8ed58fb211893e0c2675b8eec
Nikolay Moriakov <koly86@gmail.com>
2018-03-15 20:12:37 +0300

    Vell done

--- 14,03,2018/Program.cs
+++ 14,03,2018/Program.cs
@@ -8,10 +8,37 @@ namespace _14_03_2018
 {
     class Program
     {
+        public static int some = 0;
+        private static int result = 0;
         static int[] arr = new int[30];
         static void Main(string[] args)
         {
+            ArrayToFill();
+            //показываем массив и выявляем наибольшее число
+            foreach (var t in arr)
+            {
+                if (some < t)
+                {
+                    some = t;
+                }
 
+                Console.Write(t + ", ");
+            }
+
+            //находим количество значений равных наибольшему
+            foreach (var t2 in arr)
+            {
+                if (some == t2)
+                {
+                    result++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($@"The Biggest number in the array:{some}");
+            Console.WriteLine($@"Total number equals biggest number: {result}");
+
+            Console.ReadKey();
         }
 
 
@@ -19,7 +46,8 @@ namespace _14_03_2018
         {
             var rnd = new Random();
 
-            for (var i = 0; i < arr.Length; i++) arr[i] = rnd.Next(1, 30);
+            for (var i = 0; i < arr.Length; i++)
+                arr[i] = rnd.Next(1, 30);
         }
     }
 }
