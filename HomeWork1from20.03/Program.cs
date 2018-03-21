f9dc94fa29fb410718f3c66a98270928bb979e66
Nikolay Moriakov <koly86@gmail.com>
2018-03-21 23:52:58 +0300

    +

--- HomeWork1from20.03/Program.cs
+++ HomeWork1from20.03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 
 //Дан целочисленный массив из 40 элементов. Элементы массива могут принимать целые значения от 1 до 10000 включительно.
@@ -11,15 +12,20 @@ namespace HomeWork1from20._03
     internal class Program
     {
         private static readonly Random rnd = new Random();
-        private static readonly int[] arr = new int[40];
+        private static readonly int[] arr = new int[40000];
         private static int _max;
         public static int _min = 0;
         private static readonly char[] charNumeric = new char[10] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
         private static void Main(string[] args)
         {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
             ToFillArray();
             MyMet();
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            Console.WriteLine(ts.Milliseconds);
 
             Console.ReadKey();
         }
@@ -27,13 +33,13 @@ namespace HomeWork1from20._03
 
         private static void ToFillArray()
         {
-            for (var i = 0; i < arr.Length; i++) arr[i] = rnd.Next(2, 10000);
+            for (var i = 0; i < arr.Length; i++) arr[i] = rnd.Next(1, 10000);
         }
 
         private static void MyMet()
         {
             var count = 0;
-            Array.Sort(arr); //сортируем в сторону увеличения
+           // Array.Sort(arr); //сортируем в сторону увеличения
             for (var i = 0; i < arr.Length; i++)
             {
                 for (var j = 0; j < arr[i].ToString("X").Length; j++) //перебираем полученную цифру по слогам
---
+++ HomeWork2from20.03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2from20._03
+{
+    class Program
+    {
+        private static readonly int[] arr = new int[40000];
+        private static readonly Random rnd = new Random();
+        static void Main(string[] args)
+        {
+        }
+
+        private static void ToFillArray()
+        {
+            for (var i = 0; i < arr.Length; i++) arr[i] = rnd.Next(1, 10000);
+        }
+    }
+}
