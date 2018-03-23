828ecdaca0a9e8619b8389452a503d7539c6796d
Nikolay Moriakov <koly86@gmail.com>
2018-03-23 17:46:57 +0300

    HomeWork4

---
+++ HomeWork4from20.03/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace HomeWork4from20._03
+{
+    internal class Program
+    {
+        private static readonly int[] ArrayInts = new int[2017];
+        private static int[] NewArray;
+
+        private static Random _rnd = new Random();
+        private static int c;
+        private static int j;
+        private static int count;
+
+        private static void Main(string[] args)
+        {
+            ToFill();
+            Resolve();
+            Console.ReadKey();
+        }
+
+        private static void ToFill()
+        {
+            _rnd = new Random();
+
+            for (var i = 0; i < ArrayInts.Length; i++) ArrayInts[i] = _rnd.Next(1, 2000); //Заполняем массив
+            Array.Sort(ArrayInts); // сортируем
+        }
+
+        private static void Resolve()
+        {
+            Console.WriteLine($@"OriginalArray      NewArray");
+            foreach (var t in ArrayInts)
+            {
+                Console.WriteLine($@" T = {t}          ");
+                if (t == c)
+                {
+                    
+                    continue;
+                }
+                c = t;
+                    Console.WriteLine($@"                     C = {c}");
+                count++; //считаем уникальные числа
+            }
+
+
+            NewArray = new int[count]; // создаем новый массив
+            foreach (var t in ArrayInts)
+            {
+                if (t == c) continue;
+                c = t;
+                NewArray[j++] = t; 
+            }
+        }
+    }
+}
---
+++ HomwWork3from20.03/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomwWork3from20._03
+{
+    class Program
+    {
+
+        private static readonly Random rnd = new Random();
+        private static readonly int[] arr = new int[40];
+        private static int _max;
+        public static int _min = 0;
+        private static readonly char[] charNumeric = new char[10] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private static readonly char[] charSymbol = new char[6] {'A', 'B', 'C', 'D', 'E', 'F'};
+
+        private static void Main(string[] args)
+        {
+            ToFillArray();
+            MyMet();
+            Console.ReadKey();
+        }
+
+        private static void ToFillArray()
+        {
+            arr[0] = 524;
+            for (var i = 1; i < arr.Length; i++) arr[i] = rnd.Next(1, 10000);
+        }
+
+
+        private static void MyMet()
+        {
+            var count = 0;
+            Array.Sort(arr); //сортируем в сторону увеличения
+            for (var i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($@"Hex: {arr[i]:X}   original {arr[i]}");
+
+
+                for (var j = 0; j < arr[i].ToString("X").Length; j++) //перебираем полученную цифру по слогам
+
+                    if (arr[i].ToString("X")[arr[i].ToString("X").Length - 1] == 'C') //проверяем что последний символ С
+                    {
+                        foreach (var t in charNumeric) // сравниваем каждый элемент с чаром из массива
+
+                            if (arr[i].ToString("X")[j] == t)
+                                count++;
+                        if (count < 3) continue;
+                        Console.WriteLine($@" Number Original {arr[i]}");
+                        Console.WriteLine($@" Number in Hex {arr[i]:X}");
+                        return;
+                    }
+
+                count = 0;
+            }
+
+            if (count == 0) Console.WriteLine($@"No one have been find: 0");
+        }
+    }
+}
