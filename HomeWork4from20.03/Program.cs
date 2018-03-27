6826f85fbc184dfd640b6def7fd0ffe496a15826
Nikolay Moriakov <koly86@gmail.com>
2018-03-27 21:42:06 +0300

    da

--- HomeWork4from20.03/Program.cs
+++ HomeWork4from20.03/Program.cs
@@ -4,7 +4,7 @@ namespace HomeWork4from20._03
 {
     internal class Program
     {
-        private static readonly int[] ArrayInts = new int[2017];
+        private static readonly int[] ArrayInts = new int[20];
         private static int[] NewArray;
 
         private static Random _rnd = new Random();
@@ -15,6 +15,7 @@ namespace HomeWork4from20._03
         private static void Main(string[] args)
         {
             ToFill();
+            Resolve();
             Console.ReadKey();
         }
 
@@ -22,8 +23,32 @@ namespace HomeWork4from20._03
         {
             _rnd = new Random();
 
-            for (var i = 0; i < ArrayInts.Length; i++) ArrayInts[i] = _rnd.Next(1, 2000); //Заполняем массив
+            for (var i = 0; i < ArrayInts.Length; i++) ArrayInts[i] = _rnd.Next(1, 100); //Заполняем массив
             Array.Sort(ArrayInts); // сортируем
+        }
+
+        private static void Resolve()
+        {
+            Console.WriteLine($@"OriginalArray      NewArray");
+            foreach (var t in ArrayInts)
+            {
+                Console.WriteLine($@" T = {t}          ");
+                if (t == c) continue;
+                c = t;
+                Console.WriteLine($@"                     C = {c}");
+                count++; //считаем уникальные числа
+            }
+
+
+            NewArray = new int[count]; // создаем новый массив
+            foreach (var t in ArrayInts)
+            {
+                if (t == c) continue;
+                c = t;
+                NewArray[j++] = t;
+            }
+
+            Console.WriteLine($@"количество уникальных чисел = {count}");
         }
     }
 }
